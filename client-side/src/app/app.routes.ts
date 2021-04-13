0fb3b7bc2fc472f70b27fa55b8f6a713617a3ba9
Shir Ohana <shir.o@pepperi.com>
2021-04-13 19:46:06 +0300

    ui

--- client-side/src/app/app.routes.ts
+++ client-side/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmptyRouteComponent } from './components/empty-route/empty-route.component';
-import { DataIndexComponent } from './data-index/data-index.component';
+import { DataIndexComponent } from './components/data-index/data-index.component';
 // import * as config from '../../../addon.config.json';
 
 const routes: Routes = [
